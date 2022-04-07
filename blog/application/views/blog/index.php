94b311860c0c97d157fb88bbac6f0494f05fb28b
Fathiya Anandita <fathiyaanandita1@gmail.com>
2022-04-07 13:07:29 +0700

    menambahkan latihan praktikum blog

---
+++ blog/application/views/blog/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman index</title>
+</head>
+<body>
+    <h2>Halaman Belajar Blog menggunakan <?php echo $nama ?></h2>
+    <p>ini adalah blog pertama saya</p>
+</body>
+</html>
